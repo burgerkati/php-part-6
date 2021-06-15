ac1e8e4f1955da473888a80237ec7886e0cc7631
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-15 15:21:24 +0200

    reverse variable output

--- exercice_1/verify.php
+++ exercice_1/verify.php
@@ -3,10 +3,10 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     // collect value of input field
     $surname = $_POST['surname'];
     $name = $_POST['name'];
-    if (empty($surname && $name)) {
+    if (empty($name && $surname)) {
         echo "Veuillez saisir vos informations.";
     } else {
-        echo htmlspecialchars($surname . $name);
+        echo htmlspecialchars('Bienvenue '. $name . ' ' . $surname);
     }
 }
 ?>
