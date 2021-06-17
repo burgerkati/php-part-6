1d5bc5540021fca648f7631b2eb01ef1937e6bb6
katherineburgain <68737754+burgerkati@users.noreply.github.com>
2021-06-17 09:35:55 +0200

    add exercice/it works

---
+++ exercice_2/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="fr" dir="ltr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <!-- <link rel="stylesheet" href="style.css"> -->
+  <title>PHP pt.6 exercice_2</title>
+</head>
+<body>
+  <form class="" action="verify.php" method="post">
+    <label for="surname">Nom :</label>
+    <input id="surname" type="text" name="surname" placeholder="Nom">
+    <label for="name">Prénom :</label>
+    <input id="name" type="text" name="name" placeholder="Prénom">
+    <button type="submit" name="button">Valider</button>
+  </form>
+</body>
+</html>
